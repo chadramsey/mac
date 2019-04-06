371447e984c5d58aac5f57bcfb5fda05789537b5
chadramsey <ramseyca@gmail.com>
2019-04-06 10:49:55 -0400

    Updates to structure and fixes

---
+++ src/ClickerRunnable.java
@@ -0,0 +1,41 @@
+import java.awt.*;
+import java.awt.event.MouseEvent;
+
+public class ClickerRunnable implements Runnable {
+    private Robot macro;
+    private volatile boolean shutdown = false;
+
+    public void stop() {
+        System.out.println("Stopping");
+        shutdown = true;
+    }
+
+    @Override
+    public void run() {
+        createMacro();
+        while (!shutdown) {
+            delay(2.5);
+            if (!shutdown) {
+                leftClick();
+            }
+        }
+    }
+
+    private void delay(double seconds) {
+        macro.delay((int)(seconds * 1000.0));
+    }
+
+    private void leftClick() {
+        System.out.println("Click");
+        macro.mousePress(MouseEvent.BUTTON1_DOWN_MASK);
+        macro.mouseRelease(MouseEvent.BUTTON1_DOWN_MASK);
+    }
+
+    private void createMacro() {
+        try {
+            macro = new Robot();
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+    }
+}
